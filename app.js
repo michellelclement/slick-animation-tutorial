58ce400de7f7e03b0a3bc088b21ed9ebd57c6f47
Michelle Clement <michellelclement@gmail.com>
2022-06-14 10:34:57 +0000

    iPhone video turn on scroll with text appearing stagared, phone pinned

--- app.js
+++ app.js
@@ -101,3 +101,27 @@ swatches.forEach((swatch, index) => {
     currentSwatch = swatchName;
   });
 });
+
+//Page 5 video on scroll
+const tlVideo = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".fifth-page",
+    start: "0%",
+    end: "150%",
+    scrub: true,
+    pin: true
+  }
+});
+
+tlVideo.fromTo(
+  ".product-video",
+  { currentTime: 0 },
+  { currentTime: 3, duration: 2 }
+);
+
+tlVideo.fromTo(
+  ".product-info-container h3",
+  { opacity: 0 },
+  { opacity: 1, stagger: 0.25 },
+  "<"
+);
