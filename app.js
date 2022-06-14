ef07f3d0aa4616381f4b6d9111e54d6faa0b5e53
Michelle Clement <michellelclement@gmail.com>
2022-06-14 09:24:06 +0000

    Text highlight on page scroll

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
-const tlIntro = gsap.timeline({
+//Highlight page 2
+const tlH = gsap.timeline({
   scrollTrigger: {
-    trigger: ".first-page",
-    start: "0%", //Starts animating at the start of the page.
-    end: "50%", // Ends half way through the page scroll
-    markers: true,
-    scrub: true // Fades out nav
+    trigger: ".second-page",
+    markers: { startColor: "blue", endColor: "red" },
+    scrub: true, //animates whilst scrubbing
+    start: "-40%",
+    end: "40%"
   }
 });
 
-tlIntro.fromTo("nav", { opacity: 1 }, { opacity: 0 }); //Fades out nav on scroll
+//Highlight out
+const tlHRemove = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".second-page",
+    markers: { startColor: "pink", endColor: "pink" },
+    scrub: true, //animates whilst scrubbing
+    start: "-20%",
+    end: "60%"
+  }
+});
+
+tlH.fromTo(
+  ".highlight",
+  { color: "rgba(255,255,255, 0.4)" },
+  { color: "rgba(255,255,255, 1)", stagger: 1 }
+);
+
+tlHRemove.to(".highlight", { color: "rgba(255,255,255, 0.4)", stagger: 1 });
